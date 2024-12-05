5e677458197a2d4d483c517965e52079ffaebc39
Marcelo Asmat <marceloasmat@Marcelos-MacBook-Air.attlocal.net>
2024-12-05 07:20:19 -0600

    added empty activity android project

---
+++ android-app/app/src/main/java/com/marceloasmat/asvabsuccess/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.marceloasmat.asvabsuccess
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.marceloasmat.asvabsuccess.ui.theme.AsvabSuccessTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AsvabSuccessTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AsvabSuccessTheme {
+        Greeting("Android")
+    }
+}
