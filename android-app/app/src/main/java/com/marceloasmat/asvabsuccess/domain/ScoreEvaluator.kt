b76d556131a7568cb752728ab5c8702b60acf7c9
Marcelo Asmat <>
2024-12-18 06:45:54 -0600

    fixed return statement

--- android-app/app/src/main/java/com/marceloasmat/asvabsuccess/domain/ScoreEvaluator.kt
+++ android-app/app/src/main/java/com/marceloasmat/asvabsuccess/domain/ScoreEvaluator.kt
@@ -1,10 +1,12 @@
 package com.marceloasmat.asvabsuccess.domain
 
-class ScoreEvaluator{
+class ScoreEvaluator {
     fun evaluate(userTestResults: UserTestResults, navyRate: NavyRate): Boolean {
         var results = 0
         navyRate.qualifyingCondition.topics.forEach { topic ->
             val score = userTestResults.scores.find { it.sectionTopic == topic }?.score ?: 0
             results += score
-        54  }
-    }12
+        }
+        return results >= navyRate.qualifyingCondition.minimumScore
+    }
+}
