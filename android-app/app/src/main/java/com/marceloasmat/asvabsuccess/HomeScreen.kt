ed52dda18ecdcb6594325eb3d0cca318b3b4b39c
Marcelo Asmat <marceloasmat@gmail.com>
2025-08-11 19:55:16 -0500

    center aligned text labels

--- android-app/app/src/main/java/com/marceloasmat/asvabsuccess/HomeScreen.kt
+++ android-app/app/src/main/java/com/marceloasmat/asvabsuccess/HomeScreen.kt
@@ -4,19 +4,24 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.marceloasmat.asvabsuccess.ui.theme.AsvabSuccessTheme
 
 @Composable
 fun HomeScreen() {
-    Column(modifier = Modifier.fillMaxSize()) {
-        Text(text = "ASVAB Calculator")
-        Text(text = "Enter your scores")
+    Column(modifier = Modifier.fillMaxSize().statusBarsPadding()) {
+        Text(text = "ASVAB Calculator", fontSize = 40.sp, textAlign = TextAlign.Center, modifier = Modifier.fillMaxWidth() )
+        Text(text = "Enter your scores", fontSize = 25.sp, textAlign = TextAlign.Center, modifier = Modifier.fillMaxWidth())
         InputScores()
+        CalculateButton()
     }
 }
 
@@ -35,6 +40,15 @@ fun InputScores(){
             UserInputCell()
             UserInputCell()
         }
+    }
+}
+
+@Composable
+fun CalculateButton(){
+    Button(
+        onClick = {}
+    ) {
+        Text(text = "Calculate")
     }
 }
 
