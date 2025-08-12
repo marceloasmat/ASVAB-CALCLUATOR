853feecdac716fb72f003f40f820fd8d8c6e36ce
Marcelo Asmat <marceloasmat@gmail.com>
2025-08-12 08:22:31 -0500

    aligned textboxes and labels

--- android-app/app/src/main/java/com/marceloasmat/asvabsuccess/HomeScreen.kt
+++ android-app/app/src/main/java/com/marceloasmat/asvabsuccess/HomeScreen.kt
@@ -1,10 +1,14 @@
 package com.marceloasmat.asvabsuccess
 
+import android.media.SubtitleData
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -12,12 +16,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.marceloasmat.asvabsuccess.ui.theme.AsvabSuccessTheme
 
 @Composable
 fun HomeScreen() {
-    Column(modifier = Modifier.fillMaxSize().statusBarsPadding()) {
+    Column(modifier = Modifier.fillMaxSize().statusBarsPadding().padding(horizontal = 16.dp)) {
         Text(text = "ASVAB Calculator", fontSize = 40.sp, textAlign = TextAlign.Center, modifier = Modifier.fillMaxWidth() )
         Text(text = "Enter your scores", fontSize = 25.sp, textAlign = TextAlign.Center, modifier = Modifier.fillMaxWidth())
         InputScores()
@@ -27,20 +32,18 @@ fun HomeScreen() {
 
 @Composable
 fun InputScores(){
-    Row(modifier = Modifier.fillMaxWidth()) {
-        Column(modifier = Modifier.weight(1f)) {
-            UserInputCell()
-            UserInputCell()
-            UserInputCell()
-            UserInputCell()
-        }
-        Column(modifier = Modifier.weight(1f)) {
-            UserInputCell()
-            UserInputCell()
-            UserInputCell()
-            UserInputCell()
-        }
+    Column(modifier = Modifier, verticalArrangement = Arrangement.spacedBy(10.dp) // Adds 10dp of vertical space between children
+    ) {
+        UserInputCell("AR", "Arithmetic")
+        UserInputCell(title = "GS", subtitle = "General Science")
+        UserInputCell(title = "MK", subtitle = "Math Knowledge")
+        UserInputCell(title = "MC", subtitle = "Mechanical Comprehension")
+        UserInputCell(title = "El", subtitle = "Electrical Knowledge")
+        UserInputCell(title = "WK", subtitle = "Word Knowledge")
+        UserInputCell(title = "PC", subtitle = "Paragraph Comprehension")
+        UserInputCell(title = "VE", subtitle = "Verbal Expression")
     }
+
 }
 
 @Composable
@@ -53,15 +56,17 @@ fun CalculateButton(){
 }
 
 @Composable
-fun UserInputCell(){
+fun UserInputCell(title: String, subtitle: String){
     Row {
-        Column {
-            Text(text = "AR")
-            Text(text = "Arithmetic")
+        Column(modifier = Modifier.weight(1f)) {
+            Text(text = title, fontSize = 20.sp, textAlign = TextAlign.Center,)
+            Text(text = subtitle, fontSize = 13.sp, )
         }
         TextField(
             value = "",
-            onValueChange = {}
+            onValueChange = {},
+            modifier = Modifier.width(60.dp)
+
         )
     }
 }
