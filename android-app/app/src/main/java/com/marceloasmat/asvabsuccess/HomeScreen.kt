7de815a76fd8707d80fb5ab5831285fff229d670
Marcelo Asmat <marceloasmat@gmail.com>
2025-08-11 19:40:16 -0500

    added weight to user input columns

--- android-app/app/src/main/java/com/marceloasmat/asvabsuccess/HomeScreen.kt
+++ android-app/app/src/main/java/com/marceloasmat/asvabsuccess/HomeScreen.kt
@@ -2,15 +2,18 @@ package com.marceloasmat.asvabsuccess
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.marceloasmat.asvabsuccess.ui.theme.AsvabSuccessTheme
 
 @Composable
 fun HomeScreen() {
-    Column {
+    Column(modifier = Modifier.fillMaxSize()) {
         Text(text = "ASVAB Calculator")
         Text(text = "Enter your scores")
         InputScores()
@@ -19,14 +22,14 @@ fun HomeScreen() {
 
 @Composable
 fun InputScores(){
-    Row {
-        Column {
+    Row(modifier = Modifier.fillMaxWidth()) {
+        Column(modifier = Modifier.weight(1f)) {
             UserInputCell()
             UserInputCell()
             UserInputCell()
             UserInputCell()
         }
-        Column {
+        Column(modifier = Modifier.weight(1f)) {
             UserInputCell()
             UserInputCell()
             UserInputCell()
