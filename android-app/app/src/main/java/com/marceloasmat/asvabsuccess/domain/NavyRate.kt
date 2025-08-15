41889d5c259765d51b72030ccbdd669d16ef52f5
Marcelo Asmat <marceloasmat@gmail.com>
2025-08-15 17:53:57 -0500

    begain creating enum classes for rates.

--- android-app/app/src/main/java/com/marceloasmat/asvabsuccess/domain/NavyRate.kt
+++ android-app/app/src/main/java/com/marceloasmat/asvabsuccess/domain/NavyRate.kt
@@ -1,6 +1,9 @@
 package com.marceloasmat.asvabsuccess.domain
 
-data class NavyRate(
-    val name: String,
-    val qualifyingCondition: QualifyingCondition,
-)
+enum class NavyRate(val nameLabel: String) {
+    AviationBoatswainMate(nameLabel = "Aviation Boatswain Mate"),
+    AviationMachinistMate(nameLabel = "Aviation Machinist's Mate"),
+    AviationSupportEquipment(nameLabel = "Aviation Support Equipment Technician"),
+    NavalAircrewman(nameLabel = "Naval Aircrewman"),
+    AircrewRescueSwimmer(nameLabel = "Aircrew Rescue Swimmer")
+}
